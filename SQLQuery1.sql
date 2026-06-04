ba220267004afe259566907eb0e3794ca0581bd5
kdcar15 <caaguirre@uamv.edu.ni>
2026-06-04 14:14:01 -0600

    Crear tablas

--- SQLQuery1.sql
+++ SQLQuery1.sql
@@ -6,3 +6,55 @@ GO
 
 USE HospitalDB
 GO
+
+-- Creación de tablas
+CREATE TABLE Pacientes
+(
+	idPaciente int,
+	nombres nvarchar(100),
+	apellidos nvarchar(100),
+	fechaNac date,
+	habitacion int
+)
+
+CREATE TABLE Medicos(
+	idMedico int,
+	nombres nvarchar(100),
+	apellidos nvarchar(100),
+	fechaNac date,
+	especialidad int
+)
+
+CREATE TABLE Especialidades(
+	idEspecialidad int,
+	nombre nvarchar(100)
+)
+
+CREATE TABLE Citas(
+	idCita int,
+	fechaHora datetime,
+	paciente int,
+	medico int,
+)
+
+CREATE TABLE Habitaciones(
+	idHabitacion int,
+	codigo nvarchar(50),
+)
+
+CREATE TABLE Tratamientos(
+	idTratamiento int,
+	idPaciente int,
+)
+
+CREATE TABLE Medicamentos(
+	idMedicamento int,
+	nombre nvarchar(50)
+)
+
+CREATE TABLE DetallesTratamientos(
+	idTratamiento int,
+	idMedicamento int,
+	dosis float
+)
+
